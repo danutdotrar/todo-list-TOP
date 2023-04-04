ed60a42bf719cc70bbae8a436b099d890159ccc3
danutdotrar <morarasu.danut@gmail.com>
2023-04-04 20:57:35 +0300

    Add NPM watch and HTMLWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
+    mode: "development",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: path.resolve(__dirname, "src/index.html"),
+            filename: "index.html",
+        }),
+    ],
     module: {
         rules: [
             {
