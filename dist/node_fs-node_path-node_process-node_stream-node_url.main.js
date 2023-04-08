ea32d3705149074cf47a26d8ca701d92e7eb3ec7
danutdotrar <morarasu.danut@gmail.com>
2023-04-08 21:32:30 +0300

    Add project name from modal-project

---
+++ dist/node_fs-node_path-node_process-node_stream-node_url.main.js
@@ -0,0 +1,81 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunktodo_list_top"] = self["webpackChunktodo_list_top"] || []).push([["node_fs-node_path-node_process-node_stream-node_url"],{
+
+/***/ "?538f":
+/*!**********************!*\
+  !*** path (ignored) ***!
+  \**********************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://todo-list-top/path_(ignored)?");
+
+/***/ }),
+
+/***/ "?54c0":
+/*!********************!*\
+  !*** fs (ignored) ***!
+  \********************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://todo-list-top/fs_(ignored)?");
+
+/***/ }),
+
+/***/ "node:fs":
+/*!***************!*\
+  !*** node:fs ***!
+  \***************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed: UnhandledSchemeError: Reading from \\\"node:fs\\\" is not handled by plugins (Unhandled scheme).\\nWebpack supports \\\"data:\\\" and \\\"file:\\\" URIs by default.\\nYou may need an additional plugin to handle \\\"node:\\\" URIs.\\n    at C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:835:25\\n    at Hook.eval [as callAsync] (eval at create (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:6:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:18:14)\\n    at Object.processResource (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:832:8)\\n    at processResource (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:220:11)\\n    at iteratePitchingLoaders (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:171:10)\\n    at runLoaders (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:398:2)\\n    at NormalModule._doBuild (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:822:3)\\n    at NormalModule.build (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:966:15)\\n    at C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:1371:12\");\n\n//# sourceURL=webpack://todo-list-top/node:fs?");
+
+/***/ }),
+
+/***/ "node:path":
+/*!*****************!*\
+  !*** node:path ***!
+  \*****************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed: UnhandledSchemeError: Reading from \\\"node:path\\\" is not handled by plugins (Unhandled scheme).\\nWebpack supports \\\"data:\\\" and \\\"file:\\\" URIs by default.\\nYou may need an additional plugin to handle \\\"node:\\\" URIs.\\n    at C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:835:25\\n    at Hook.eval [as callAsync] (eval at create (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:6:1)\\n    at Object.processResource (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:832:8)\\n    at processResource (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:220:11)\\n    at iteratePitchingLoaders (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:171:10)\\n    at runLoaders (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:398:2)\\n    at NormalModule._doBuild (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:822:3)\\n    at NormalModule.build (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:966:15)\\n    at C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:1371:12\\n    at NormalModule.needBuild (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:1254:32)\");\n\n//# sourceURL=webpack://todo-list-top/node:path?");
+
+/***/ }),
+
+/***/ "node:process":
+/*!********************!*\
+  !*** node:process ***!
+  \********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed: UnhandledSchemeError: Reading from \\\"node:process\\\" is not handled by plugins (Unhandled scheme).\\nWebpack supports \\\"data:\\\" and \\\"file:\\\" URIs by default.\\nYou may need an additional plugin to handle \\\"node:\\\" URIs.\\n    at C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:835:25\\n    at Hook.eval [as callAsync] (eval at create (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:6:1)\\n    at Object.processResource (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:832:8)\\n    at processResource (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:220:11)\\n    at iteratePitchingLoaders (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:171:10)\\n    at runLoaders (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:398:2)\\n    at NormalModule._doBuild (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:822:3)\\n    at NormalModule.build (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:966:15)\\n    at C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:1371:12\\n    at NormalModule.needBuild (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:1254:32)\");\n\n//# sourceURL=webpack://todo-list-top/node:process?");
+
+/***/ }),
+
+/***/ "node:stream":
+/*!*******************!*\
+  !*** node:stream ***!
+  \*******************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed: UnhandledSchemeError: Reading from \\\"node:stream\\\" is not handled by plugins (Unhandled scheme).\\nWebpack supports \\\"data:\\\" and \\\"file:\\\" URIs by default.\\nYou may need an additional plugin to handle \\\"node:\\\" URIs.\\n    at C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:835:25\\n    at Hook.eval [as callAsync] (eval at create (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:6:1)\\n    at Object.processResource (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:832:8)\\n    at processResource (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:220:11)\\n    at iteratePitchingLoaders (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:171:10)\\n    at runLoaders (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:398:2)\\n    at NormalModule._doBuild (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:822:3)\\n    at NormalModule.build (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:966:15)\\n    at C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:1371:12\\n    at NormalModule.needBuild (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:1254:32)\");\n\n//# sourceURL=webpack://todo-list-top/node:stream?");
+
+/***/ }),
+
+/***/ "node:url":
+/*!****************!*\
+  !*** node:url ***!
+  \****************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed: UnhandledSchemeError: Reading from \\\"node:url\\\" is not handled by plugins (Unhandled scheme).\\nWebpack supports \\\"data:\\\" and \\\"file:\\\" URIs by default.\\nYou may need an additional plugin to handle \\\"node:\\\" URIs.\\n    at C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:835:25\\n    at Hook.eval [as callAsync] (eval at create (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:6:1)\\n    at Object.processResource (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:832:8)\\n    at processResource (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:220:11)\\n    at iteratePitchingLoaders (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:171:10)\\n    at runLoaders (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js:398:2)\\n    at NormalModule._doBuild (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:822:3)\\n    at NormalModule.build (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:966:15)\\n    at C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:1371:12\\n    at NormalModule.needBuild (C:\\\\Users\\\\Asus\\\\Desktop\\\\Projects\\\\todo-list-TOP\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js:1254:32)\");\n\n//# sourceURL=webpack://todo-list-top/node:url?");
+
+/***/ })
+
+}]);
--- src/index.js
+++ src/index.js
@@ -15,13 +15,12 @@ const modalProject = document.getElementById("modal-project-container");
 const projectList = document.getElementById("project-list");
 
 // Add project modal
-const modalAddProject = document.getElementById("modal-project");
+const modalAddProject = document.getElementById("modal-project-container");
 const addProjectName = document.getElementById("add-project-name");
 
-// Cream un proiect (obiect) care va contine o lista (obiect) care va contine un todo task (obiect)
+let taskContent;
 
-// Cream todo task object
-// Ii vom da push in lista de obiecte
+// Create todo object
 class Todo {
     constructor(title, dueDate, priority) {
         this.title = title;
@@ -30,11 +29,58 @@ class Todo {
     }
 }
 
-// Update
-class TodoList {
-    constructor() {
-        this.todoTaskList = [];
+// Create list of todos
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.tasks = [];
     }
+
+    // Add todo task to the project array
+    addTask(title, dueDate, priority) {
+        const todo = new Todo(title, dueDate, priority);
+        this.tasks.push(todo);
+    }
+}
+
+// Create a list of projects
+class ProjectList {
+    constructor() {
+        this.projects = [];
+    }
+
+    // Add project to the projects list array
+    addProject(project) {
+        this.projects.push(project);
+    }
+}
+
+// Create a new project list
+let myProjectList = new ProjectList();
+
+// Create a new project name
+let project1 = new Project("1 test project");
+let project2 = new Project("2 test project");
+
+// Add tasks to project1
+project1.addTask("1 Task test for project 1", "12/12/1222", "high");
+project1.addTask("2 Task test for project 1", "12/12/1222", "high");
+console.log(project1);
+
+// Add project1 to myProjectList
+myProjectList.addProject(project1);
+myProjectList.addProject(project2);
+console.log(myProjectList);
+
+for (let i = 0; i < myProjectList.projects.length; i++) {
+    console.log(`Project ${i + 1}: ${myProjectList.projects[i].name}`);
+    console.log("Tasks:");
+    for (let j = 0; j < myProjectList.projects[i].tasks.length; j++) {
+        console.log(
+            `- ${myProjectList.projects[i].tasks[j].title} (${myProjectList.projects[i].tasks[j].dueDate})`
+        );
+    }
+    console.log("");
 }
 
 // Modal class
@@ -54,28 +100,81 @@ class ModalProjects {
     constructor() {}
 
     openProjectModal() {
-        modalAddProject.projectModal.classList.remove("hidden");
+        modalAddProject.classList.remove("hidden");
     }
 
     closeProjectModal() {
-        modalAddProject.projectModal.classList.add("hidden");
+        modalAddProject.classList.add("hidden");
     }
 }
 
+// Create new modals
 const mainModal = new Modal();
+const projectModal = new ModalProjects();
 
+// Event listeners
 addTask.addEventListener("click", function () {
     mainModal.openModal();
 });
 
 closeBtnModal.addEventListener("click", function () {
     mainModal.closeModal();
+    form.reset();
 });
+
+let todoTaskList = new Project();
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    console.log("merege");
+
+    todoTaskList.addTask(taskName.value, datePicker.value, priorityLevel.value);
+
+    renderTasks(todoTaskList);
+
+    taskList.insertAdjacentHTML("beforeend", taskContent);
 
     mainModal.closeModal();
     form.reset();
+    console.log(todoTaskList);
+});
+
+function renderTasks(array) {
+    for (let i in array) {
+        taskContent = `                        <div class="todo-task" id="todo-task" data-todoId=${i}>
+        <div class="left-part">
+            <input type="radio" name="" />
+            <div class="todo-task-name" id="todo-task-name">
+                ${taskName.value}
+            </div>
+        </div>
+
+        <div class="right-part">
+            <div class="priority-level" id="priority-level">
+                ${priorityLevel.value}
+            </div>
+            <input type="date" />
+            <div class="delete-task" id="delete-task">
+                &times;
+            </div>
+        </div>
+    </div>`;
+    }
+    return taskContent;
+}
+
+// Add project and project name
+addProject.addEventListener("click", function () {
+    projectModal.openProjectModal();
+});
+
+addProjectName.addEventListener("click", function () {
+    // Add new project to myProjectList
+    myProjectList.addProject(projectName.value);
+
+    // Close the modal
+    projectModal.closeProjectModal();
+
+    // Clear the modal
+    projectName.value = "";
+    console.log(myProjectList);
 });
